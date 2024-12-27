a44033eeb7f1d28d7165a21653bea0abdd3823e2
Adarsh Hiwrale <ahiwrale@sigmasolve.net>
2024-12-27 14:22:53 +0530

    managed scope down statement

--- variables.tf
+++ variables.tf
@@ -15,50 +15,59 @@ variable "managed_rules" {
     override_action = string
     vendor_name     = string
     version         = optional(string)
-    rule_action_override = list(object({
+    rule_action_override = optional(list(object({
       name          = string
-      action_to_use = string
-    }))
+      action_to_use = string # e.g., "allow", "block", "count"
+    })), [])
+    scope_down_statement = optional(object({
+      ip_set_arn = string
+    }), null)
   }))
+
   description = "List of Managed WAF rules."
   default = [
     {
-      name                 = "AWSManagedRulesCommonRuleSet",
+      name                 = "AWSManagedRulesCommonRuleSet"
       priority             = 10
       override_action      = "none"
       vendor_name          = "AWS"
       rule_action_override = []
+      scope_down_statement = null
     },
     {
-      name                 = "AWSManagedRulesAmazonIpReputationList",
+      name                 = "AWSManagedRulesAmazonIpReputationList"
       priority             = 20
       override_action      = "none"
       vendor_name          = "AWS"
       rule_action_override = []
+      scope_down_statement = null
     },
     {
-      name                 = "AWSManagedRulesKnownBadInputsRuleSet",
+      name                 = "AWSManagedRulesKnownBadInputsRuleSet"
       priority             = 30
       override_action      = "none"
       vendor_name          = "AWS"
       rule_action_override = []
+      scope_down_statement = null
     },
     {
-      name                 = "AWSManagedRulesSQLiRuleSet",
+      name                 = "AWSManagedRulesSQLiRuleSet"
       priority             = 40
       override_action      = "none"
       vendor_name          = "AWS"
       rule_action_override = []
+      scope_down_statement = null
     },
     {
-      name                 = "AWSManagedRulesLinuxRuleSet",
+      name                 = "AWSManagedRulesLinuxRuleSet"
       priority             = 50
       override_action      = "none"
       vendor_name          = "AWS"
       rule_action_override = []
+      scope_down_statement = null
     },
     {
-      name                 = "AWSManagedRulesUnixRuleSet",
+      name                 = "AWSManagedRulesUnixRuleSet"
       priority             = 60
       override_action      = "none"
       vendor_name          = "AWS"
@@ -66,13 +75,13 @@ variable "managed_rules" {
       scope_down_statement = null
     },
     {
-      name                 = "AWSManagedRulesAnonymousIpList",
+      name                 = "AWSManagedRulesAnonymousIpList"
       priority             = 70
       override_action      = "none"
       vendor_name          = "AWS"
       rule_action_override = []
       scope_down_statement = null
-    },
+    }
   ]
 }
 
